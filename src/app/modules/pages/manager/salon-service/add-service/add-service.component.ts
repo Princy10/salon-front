97a7ef16ddb6dd1f2b8e0009347b60f4401a1ba6
Princy10 <rafamatanantsoa10@gmail.com>
2024-02-09 23:27:30 +0300

    HTTP async on update and delete service

--- src/app/modules/pages/manager/salon-service/add-service/add-service.component.ts
+++ src/app/modules/pages/manager/salon-service/add-service/add-service.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SalonService } from 'src/app/modules/services/salon/salon.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-service',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-service.component.css']
 })
 export class AddServiceComponent {
+  //services: any[] = []
+  serviceData = {
+    titre: '',
+    prix: 0,
+    durer: 0,
+    commission: 0
+  };
+
+  constructor(
+    private serciceSalon: SalonService,
+    private router: Router) {}
+
+  ajout_service() {
+    this.serciceSalon.createService(this.serviceData).subscribe(response => {
+      console.log('Insertion réussie', response);
+      this.router.navigate(['/list-service']);
+    }, error => {
+      console.error('Erreur lors de l\'insertion', error);
+    });
+  }
 
 }
